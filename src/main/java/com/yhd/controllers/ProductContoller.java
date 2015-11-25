dda11f6a414dc8ce1d908201a5cb7c7a1375595a
Ricky Luu <Ricky Luu>
2015-11-25 19:19:46 +0000

    product cat

--- src/main/java/com/yhd/controllers/ProductContoller.java
+++ src/main/java/com/yhd/controllers/ProductContoller.java
@@ -3,24 +3,22 @@ package com.yhd.controllers;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.yhd.DataConfig;
 import com.yhd.RepositoryConfig;
-import com.yhd.repoimpl.ProductRepo;
 import com.yhd.repos.ProductResository;
 
 @Controller
-public class IndexController {
-	
+public class ProductContoller {
+
 	ApplicationContext mongoContext = new AnnotationConfigApplicationContext(DataConfig.class, RepositoryConfig.class);
 	ProductResository productRepository = mongoContext.getBean(ProductResository.class);
 	
-	@RequestMapping("/")
-	public ModelAndView load (ModelAndView model){
-		model.setViewName("index.jsp");
-		model.addObject("title", "Your house Direct");
+	public ModelAndView load(ModelAndView model){
+		
+		
+		model.setViewName("product.jsp");
 		return model;
 		
 	}
