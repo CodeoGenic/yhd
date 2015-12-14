55f83c486db3da8a8617a0602a242b7c6f48d30b
Marcus Rodney <marcus.rodney@hotmail.co.uk>
2015-12-14 16:23:19 +0000

    login controller

---
+++ src/main/java/com/yhd/controllers/loginController.java
@@ -0,0 +1,25 @@
+package com.yhd.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+public class loginController {
+
+	@RequestMapping(value="user_login" ,method = RequestMethod.POST)
+	public ModelAndView login(ModelAndView model, HttpServletRequest request){
+		String username = (String) request.getParameter("username");
+		String password = (String) request.getParameter("password");
+		printAll(username,password);
+		return model;
+	}
+	
+	public void printAll(String ...args){
+		for(int i=0; i<args.length; i++){
+			System.out.println(args[i]);
+		}
+	}
+}
--- src/main/webapp/Navbar.jsp
+++ src/main/webapp/Navbar.jsp
@@ -124,7 +124,7 @@
                       
                        
                     </form>
-                    <form action="/" class="navbar-form navbar-right">
+                    <form action="user_login" class="navbar-form navbar-right">
                      <div class ="input-group">
 					 <input class="account" type="text" name="Username" placeholder="Username">
 					  <input class="account" type ="password" name="Password" placeholder="Password">
