618768d2d668f8d24dd40bceed8181c521834694
Marcus Rodney <marcus.rodney@hotmail.co.uk>
2015-12-22 17:49:55 +0000

    produts added

--- src/main/java/com/yhd/controllers/IndexController.java
+++ src/main/java/com/yhd/controllers/IndexController.java
@@ -11,6 +11,7 @@ import com.yhd.RepositoryConfig;
 import com.yhd.entities.Product;
 import com.yhd.repos.ProductResository;
 /*import com.yhd.util.Dimension;*/
+import com.yhd.util.Dimension;
 
 @Controller
 public class IndexController {
@@ -163,6 +164,61 @@ public class IndexController {
 //				"4 Chest of drawers", 500, 20, 99999, "nice piece of furniture", "customeremail", "customername", 2, "two chest",
 //				"image/assets/YHD drawers/KN5W/903_0257.JPG", "image/assets/YHD drawers/KN5W/903_0261.JPG",
 //				"image/assets/YHD drawers/KN5W/903_0269.JPG"));
+		
+		//YHD Tables
+		
+		/*productRepository.insert(new Product(9, "light brown",new Dimension(30,60,40), "buy this now or else", "image/assets/YHD Tables/KN46T/903_0597.JPG",
+			"Extendable Dining table", 500, 20, 99999, "Dining Table with extending feature" , "customeremail", "customername", 3, "Dining",
+				"image/assets/YHD Tables/KN46T/903_0600.JPG", "image/assets/YHD Tables/KN46T/903_0611.JPG", "image/assets/YHD Tables/KN46T/903_0624.JPG", 
+				"image/assets/YHD Tables/KN46T/903_0627.JPG", "image/assets/YHD Tables/KN46T/903_0629.JPG", "image/assets/YHD Tables/KN46T/903_0631.JPG", "image/assets/YHD Tables/KN46T/903_0633.JPG",
+				"image/assets/YHD Tables/KN46T/903_0636.JPG","image/assets/YHD Tables/KN46T/903_0638.JPG","image/assets/YHD Tables/KN46T/903_0640.JPG","image/assets/YHD Tables/KN46T/903_0642.JPG",
+				"image/assets/YHD Tables/KN46T/903_0645.JPG", "image/assets/YHD Tables/KN46T/903_0647.JPG", "image/assets/YHD Tables/KN46T/903_0648.JPG", "image/assets/YHD Tables/KN46T/903_0651.JPG",
+				"image/assets/YHD Tables/KN46T/903_0655.JPG", "image/assets/YHD Tables/KN46T/903_0659.JPG", "image/assets/YHD Tables/KN46T/903_0661.JPG", "image/assets/YHD Tables/KN46T/903_0662.JPG",
+				"image/assets/YHD Tables/KN46T/903_0670.JPG", "image/assets/YHD Tables/KN46T/903_0672.JPG", "image/assets/YHD Tables/KN46T/903_0675.JPG","image/assets/YHD Tables/KN46T/903_0679.JPG",
+				"image/assets/YHD Tables/KN46T/903_0682.JPG","image/assets/YHD Tables/KN46T/903_1340.JPG"));
+		
+		productRepository.insert(new Product(10,"light brown", new Dimension(20,60,40), "buy now", "image/assets/YHD Tables/KNCT/DPP_0012.JPG","Coffee Table", 500,20,9999, "Coffee table with lower shelf and Draws",
+									"customeremail", "customername",3, "Coffee", "image/assets/YHD Tables/KNCT/DPP_0016.JPG", "image/assets/YHD Tables/KNCT/DPP_0020.JPG",
+									"image/assets/YHD Tables/KNCT/DPP_0021.JPG", "image/assets/YHD Tables/KNCT/DPP_0027.JPG", "image/assets/YHD Tables/KNCT/DPP_0029.JPG", "image/assets/YHD Tables/KNCT/DPP_0033.JPG",
+									"image/assets/YHD Tables/KNCT/DPP_0034.JPG", "image/assets/YHD Tables/KNCT/DPP_0038.JPG","image/assets/YHD Tables/KNCT/DPP_0040.JPG","image/assets/YHD Tables/KNCT/DPP_0042.JPG"));
+		
+		productRepository.insert(new Product(11,"light brown", new Dimension(20,60,40), "buy now", "image/assets/YHD Tables/KNCT2/903_0912.JPG","Coffee Table", 500,20,9999, "Coffee table with lower shelf and Draws",
+				"customeremail", "customername",3, "Coffee", "image/assets/YHD Tables/KNCT2/903_0923.JPG", "image/assets/YHD Tables/KNCT2/903_0926.JPG",
+				"image/assets/YHD Tables/KNCT2/903_0929.JPG", "image/assets/YHD Tables/KNCT2/903_0982.JPG", "image/assets/YHD Tables/KNCT2/903_0999.JPG", "image/assets/YHD Tables/KNCT2/903_1002.JPG"));
+		
+		productRepository.insert(new Product(12,"light brown", new Dimension(20,60,40), "buy now", "image/assets/YHD Tables/KNNOT/903_0935.JPG","Coffee Table", 500,20,9999, "Coffee table with lower shelf and Draws",
+				"customeremail", "customername",3, "Coffee", "image/assets/YHD Tables/KNNOT/903_0938.JPG", "image/assets/YHD Tables/KNNOT/903_0946.JPG",
+				"image/assets/YHD Tables/KNNOT/903_0947.JPG", "image/assets/YHD Tables/KNNOT/903_0948.JPG", "image/assets/YHD Tables/KNNOT/903_0991.JPG", "image/assets/YHD Tables/KNNOT/903_1204.JPG",
+				"image/assets/YHD Tables/KNNOT/903_1207.JPG", "image/assets/YHD Tables/KNNOT/903_1231.JPG", "image/assets/YHD Tables/KNNOT/903_1235.JPG", "image/assets/YHD Tables/KNNOT/903_1238.JPG",
+				"image/assets/YHD Tables/KNNOT/903_1239.JPG"));
+		
+		productRepository.insert(new Product(13,"light brown", new Dimension(20,60,40), "buy now", "image/assets/YHD Tables/KNNS/903_0361.JPG"," Small Coffee Table", 500,20,9999, "Small Coffee table with draw",
+				"customeremail", "customername",3, "Coffee", "image/assets/YHD Tables/KNNS/903_0365.JPG", "image/assets/YHD Tables/KNNS/903_0367.JPG",
+				"image/assets/YHD Tables/KNNS/903_0369.JPG", "image/assets/YHD Tables/KNNS/903_0372.JPG", "image/assets/YHD Tables/KNNS/903_1211.JPG", "image/assets/YHD Tables/KNNS/903_1214.JPG",
+				"image/assets/YHD Tables/KNNS/903_1215.JPG"));
+		
+		productRepository.insert(new Product(14,"light brown", new Dimension(20,60,40), "buy now", "image/assets/YHD Tables/KNRET/903_1119.JPG","Oval dining Table", 500,20,9999, "Oval table with extending feature",
+				"customeremail", "customername",3, "Dining", "image/assets/YHD Tables/KNRET/903_1126.JPG", "image/assets/YHD Tables/KNRET/903_1127.JPG",
+				"image/assets/YHD Tables/KNRET/903_1138.JPG", "image/assets/YHD Tables/KNRET/903_1140.JPG", "image/assets/YHD Tables/KNRET/903_1142.JPG", "image/assets/YHD Tables/KNRET/903_1143.JPG",
+				"image/assets/YHD Tables/KNRET/903_1144.JPG", "image/assets/YHD Tables/KNRET/903_1145.JPG", "image/assets/YHD Tables/KNRET/903_1146.JPG", "image/assets/YHD Tables/KNRET/903_1147.JPG", "image/assets/YHD Tables/KNRET/903_1148.JPG",
+				"image/assets/YHD Tables/KNRET/903_1149.JPG", "image/assets/YHD Tables/KNRET/903_1150.JPG", "image/assets/YHD Tables/KNRET/903_1151.JPG", "image/assets/YHD Tables/KNRET/903_1152.JPG","image/assets/YHD Tables/KNRET/903_1153.JPG"));
+		
+		productRepository.insert(new Product(15,"light brown", new Dimension(20,60,40), "buy now", "image/assets/YHD Tables/KNSQR/903_1090.JPG","Small dining Table", 500,20,9999, "Small table no extending feature",
+				"customeremail", "customername",3, "Dining", "image/assets/YHD Tables/KNSQR/903_1092.JPG", "image/assets/YHD Tables/KNSQR/903_1096.JPG",
+				"image/assets/YHD Tables/KNSQR/903_1100.JPG", "image/assets/YHD Tables/KNSQR/903_1102.JPG", "image/assets/YHD Tables/KNSQR/903_1107.JPG", "image/assets/YHD Tables/KNSQR/903_1110.JPG",
+				"image/assets/YHD Tables/KNSQR/903_1113.JPG", "image/assets/YHD Tables/KNSQR/903_1114.JPG"));
+		
+		productRepository.insert(new Product(16,"light brown", new Dimension(20,60,40), "buy now", "image/assets/YHD Tables/RLCT/+È-Ê10360.JPG","rectangle coffee table ", 500,20,9999, "rectangle coffee table",
+				"customeremail", "customername",3, "coffee", "image/assets/YHD Tables/RLCT/+È-Ê10362.JPG", "image/assets/YHD Tables/RLCT/+È-Ê10364.JPG",
+				"image/assets/YHD Tables/RLCT/+È-Ê10366.JPG", "image/assets/YHD Tables/RLCT/+È-Ê10368.JPG", "image/assets/YHD Tables/RLCT/+È-Ê10369.JPG", "image/assets/YHD Tables/RLCT/+È-Ê10371.JPG",
+				"image/assets/YHD Tables/RLCT/+È-Ê10373.JPG", "image/assets/YHD Tables/RLCT/+È-Ê10375.JPG", "image/assets/YHD Tables/RLCT/+È-Ê10376.JPG", "image/assets/YHD Tables/RLCT/+È-Ê10378.JPG",
+				"image/assets/YHD Tables/RLCT/+È-Ê10380.JPG", "image/assets/YHD Tables/RLCT/+È-Ê10381.JPG", "image/assets/YHD Tables/RLCT/+È-Ê10385.JPG","image/assets/YHD Tables/RLCT/+È-Ê10386.JPG"));
+		
+		productRepository.insert(new Product(17,"light brown", new Dimension(20,60,40), "buy now", "image/assets/YHD Tables/RLCT2/+È-Ê10359.JPG","rectangle coffee table ", 500,20,9999, "rectangle coffee table with draws",
+				"customeremail", "customername",3, "coffee", "image/assets/YHD Tables/RLCT2/+È-Ê10463.JPG", "image/assets/YHD Tables/RLCT2/+È-Ê10464.JPG",
+				"image/assets/YHD Tables/RLCT2/+È-Ê10465.JPG", "image/assets/YHD Tables/RLCT2/+È-Ê10466.JPG", "image/assets/YHD Tables/RLCT2/+È-Ê10467.JPG", "image/assets/YHD Tables/RLCT2/+È-Ê10468.JPG",
+				"image/assets/YHD Tables/RLCT2/+È-Ê10369.JPG", "image/assets/YHD Tables/RLCT2/+È-Ê10370.JPG"));
+		*/
 		
 		
 		model.setViewName("index.jsp");
